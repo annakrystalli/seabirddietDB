7bcaed390d1df5c57a4698b9c5c2a3e96def46d9
annakrystalli <annakrystalli@googlemail.com>
2018-06-08 19:13:14 +0100

    valid proportion test

--- data-raw/01_data-process.R
+++ data-raw/01_data-process.R
@@ -1,1 +1,21 @@
+# load-libraries ----
+library(dplyr)
+
 # read-raw-data ----
+ms <- readxl::read_excel(here::here("data-raw", "Master File 20171020.xlsx")) %>% 
+    janitor::clean_names()
+
+
+
+# validate proportional frequencies
+ms %>% group_by(year, location, species, age_group, sample_size) %>% 
+    summarise(fo_o_sum = sum(fo_o)) %>% 
+    mutate(valip_prop = near(fo_o_sum, 1)) %>% arrange(desc(fo_o_sum)) %>%
+    filter(valip_prop == F) %>% 
+    readr::write_csv(here::here("data-raw", "validation", 
+                                "foo_unvalidated.csv"))
+
+
+library(dplyr)
+readr::read_csv("/Users/Anna/Documents/workflows/MERP/seabirdPrey/data-raw/validation/foo_unvalidated.csv") %>% knitr::kable()
+
