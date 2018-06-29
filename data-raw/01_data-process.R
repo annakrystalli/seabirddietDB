18a0aa6ea76cdb51ae495079da0340037b5f0808
annakrystalli <annakrystalli@googlemail.com>
2018-06-29 17:45:55 +0100

    clean up processing script

--- data-raw/01_data-process.R
+++ data-raw/01_data-process.R
@@ -5,17 +5,9 @@ library(seabirdPrey)
 seabirddiet <- readr::read_csv(
     here::here("data-raw", 
                "Seabird Diets British Isles revised 20180620.csv")) %>% 
-    janitor::clean_names() 
+    janitor::clean_names() %>%
+    bind_cols(ID =  1:nrow(seabirddiet), .)
 
-create_ID <- function(df, name, identifiers){
-    mutate = paste0()
-    df %>% dplyr::select((!!rlang::sym(identifiers[1])))
-}
-
-seabirddiet <- bind_cols(ID =  1:nrow(seabirddiet), seabirddiet)
-
-seabirddiet %>% janitor::get_dupes(startyear, location, species, prey_species,
-                                   breeding_status, age_group) %>% View()
 
 
 
