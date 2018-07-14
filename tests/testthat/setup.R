80ecb838b490403c680945e2e9f6c0773906a5a6
annakrystalli <annakrystalli@googlemail.com>
2018-07-14 12:15:12 +0100

    save testing data files to inst/testdata and load from there

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,15 +1,15 @@
 context("setup.R")
 
-taxonomy <- readr::read_tsv(here::here("data-raw", "taxonomy.tsv")) %>%
+taxonomy <- readr::read_tsv(system.file("testdata","taxonomy.tsv", 
+                                        package="seabirdPrey")) %>%
     dplyr::rename_all(dplyr::funs(stringr::str_replace(., "_prey", ""))) 
 
-taxo_correct <- readr::read_csv(here::here("data-raw", 
-                                           "worrms_nomatches_verified.csv"))
+taxo_correct <- readr::read_csv(system.file("testdata","worrms_nomatches_verified.csv", 
+                                            package="seabirdPrey"))
 
 
-seabirddiet <- readr::read_csv(
-    here::here("data-raw", 
-               "Seabird Diets British Isles revised 20180620.csv")) %>% 
+seabirddiet <- readr::read_csv(system.file("testdata","data.csv", 
+                                           package="seabirdPrey")) %>% 
     janitor::clean_names() %>%
     dplyr::bind_cols(ID =  1:nrow(.), .)
 
