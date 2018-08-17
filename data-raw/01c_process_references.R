a840b6ac238b0c4a4648b891ae9bfd512a961719
annakrystalli <annakrystalli@googlemail.com>
2018-08-17 09:14:08 +0300

    improve ref validation

--- data-raw/01c_process_references.R
+++ data-raw/01c_process_references.R
@@ -1,6 +1,5 @@
 library(dplyr)
 library(seabirdPrey)
-load(here::here("data", "seabirddiet.rda"))
 
 # read-manual-corrections ----
 # read in and post process manual corrections
@@ -17,19 +16,29 @@ ref_tidy <- tidy_refs(ref_manual)
 # get_ref_table ----
 ref <- get_ref_tbl(ref_tidy)
 
+
+
 # writeout-ref_table ----
-ref %>% readr::write_csv(here::here("data", "metadata", "references.csv"))
+ref %>% 
+    assertr::assert(assertr::is_uniq(c("ref_valid", "ref_id"))) %>%
+    assertr::assert(stringr::str_detect("ref_valid", "–") == F) %>%
+    assertr::verify(assertr::has_all_names("ref_id", "ref_valid")) %>%
+    readr::write_csv(here::here("data", "metadata", "references.csv"))
 
 # get-join_ref ----
 join_ref <- get_join_ref(ref_tidy, ref)
+join_ref <- join_ref %>% dplyr::bind_rows(
+    join_ref %>% 
+        mutate(reference = stringr::str_replace_all(reference, "–", "?")))
 
 # writeout-join_ref ----
 join_ref %>% 
     # validate
     assertr::assert(is.numeric("ref_n")) %>%
-    assertr::verify(nrow(.) == length(unique(seabirddiet$reference))) %>%
+    assertr::verify(nrow(.) == length(unique(seabirddiet$reference)) * 2) %>%
     assertr::verify(all(seabirddiet$reference %in% .$reference )) %>%
-    assertr::verify(assertr::not_na("ref_ids"))%>% 
+    assertr::verify(assertr::not_na("ref_ids")) %>% 
+    assertr::verify(assertr::has_all_names("reference", "ref_n", "ref_ids")) %>%
     # write out
     readr::write_csv(here::here("data-raw", "joins", "ref_join.csv"))
     
