e5e92c508eb3daf42e1219d6db3c223ab810279e
annakrystalli <annakrystalli@googlemail.com>
2018-08-17 09:15:22 +0300

    substitute eml workflow with completed files

--- data-raw/04_eml.R
+++ data-raw/04_eml.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(eml2)
+library(metadatar)
 
 # load-final-data ----
 seabirddiet <- readr::read_csv(
@@ -26,21 +28,22 @@ distinguished between two or more specific locations where diet samples were col
 # set-coverage-description ----
 coverage <- 
     eml2::set_coverage(begin = yr_to_isorange(seabirddiet$startyear, 
-                                        type = "start"), 
-                 end = yr_to_isorange(seabirddiet$endyear, 
-                                      type = "end"),
-                 sci_names = readr::read_csv(
-                     here::here("data", "metadata", "taxonomy.csv")),
-                 geographicDescription = geographicDescription,
-                 west = bbox["xmin"], east = bbox["xmax"], 
-                 north = bbox["ymax"], south = bbox["ymin"],
-                 altitudeMin = 0, altitudeMaximum = 0,
-                 altitudeUnits = "meter")
+                                              type = "start"), 
+                       end = yr_to_isorange(seabirddiet$endyear, 
+                                            type = "end"),
+                       sci_names = readr::read_csv(
+                           here::here("data", "metadata", "taxonomy.csv")) %>% 
+                           dplyr::select(phylum:species) %>% as.data.frame(),
+                       geographicDescription = geographicDescription,
+                       west = bbox["xmin"], east = bbox["xmax"], 
+                       north = bbox["ymax"], south = bbox["ymin"],
+                       altitudeMin = 0, altitudeMaximum = 0,
+                       altitudeUnits = "meter")
 
 
 
 # set-method ----
-methods_file <- system.file("examples/hf205-methods.docx", package = "EML")
+methods_file <- here::here("data-raw", "metadata","methods.docx")
 methods <- set_methods(methods_file)
 
 
@@ -48,22 +51,62 @@ methods <- set_methods(methods_file)
 abstract_file <-  system.file("examples/hf205-abstract.md", package = "EML")
 abstract <- set_TextType(abstract_file)
 
+
+# set-keywords
+keywordSet <- list(
+    list(
+        keywordThesaurus = "LTER controlled vocabulary",
+        keyword = list("bacteria",
+                       "carnivorous plants",
+                       "genetics",
+                       "thresholds")
+    ),
+    list(
+        keywordThesaurus = "LTER core area",
+        keyword =  list("populations", "inorganic nutrients", "disturbance")
+    ),
+    list(
+        keywordThesaurus = "HFR default",
+        keyword = list("Harvard Forest", "HFR", "LTER", "USA")
+    ))
+
+
+# set-creators
+creators_df <- readr::read_csv(here::here("data-raw", "metadata",
+                                          "creators.csv"))
+
+
+
+creators_l <- creators_df %>% apply(1, extr_creator)
+
+
+
+
+
+
+publisher <- eml$publisher(
+    organizationName = "Harvard Forest",
+    address = HF_address)
+
+contact <- 
+    list(
+        individualName = aaron$individualName,
+        electronicMailAddress = aaron$electronicMailAddress,
+        address = HF_address,
+        organizationName = creators_df$affilitation)
+
 # set-attributes ----
 set_attributes()
 
 # set-attributes ----
 
 
-
-
-
-
-my_eml <- eml$eml(
+sbd_eml <- eml$eml(
     packageId = uuid::UUIDgenerate(),  
     system = "uuid",
     dataset = eml$dataset(
-        title = "Seabird diets",
-        creator = aaron,
+        title = "Seabird Diet Database",
+        creator = creators_l,
         pubDate = "2018",
         intellectualRights = "http://www.lternet.edu/data/netpolicy.html.",
         abstract = abstract,
@@ -73,7 +116,7 @@ my_eml <- eml$eml(
         methods = methods,
         dataTable = eml$dataTable(
             entityName = "seabirddiet.csv",
-            entityDescription = "tipping point experiment 1",
+            entityDescription = "Seabird Diet Database",
             physical = physical,
             attributeList = attributeList)
     ))
