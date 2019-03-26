ae850d2263df908de9727b764ac97b59628e0b1c
Anna Krystalli <annakrystalli@googlemail.com>
2019-03-26 21:16:00 +0000

    streamline save data workflow

--- data-raw/03_save_data.R
+++ data-raw/03_save_data.R
@@ -1,4 +1,7 @@
-seabirddiet <- readr::read_csv(here::here("data-raw", "seabirddiet.csv"))
+seabirddiet <- readr::read_csv(here::here("inst", "csv", "seabirddiet.csv"), guess_max = Inf)
 
-usethis::use_data(seabirddiet)
-usethis::use_data(seabirddiet_)
+usethis::use_data(seabirddiet, overwrite = T)
+
+# sf
+seabirddiet_ <- sf::st_as_sf(seabirddiet, coords = c("latitude", "longitude"), crs = 4326) 
+usethis::use_data(seabirddiet_, overwrite = T)
