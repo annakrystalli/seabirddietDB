b5727dd23fa1ba32ece2ee0c6acd2e1e2c84f1cc
Anna Krystalli <annakrystalli@googlemail.com>
2019-03-26 21:04:59 +0000

    update processing for new database

--- data-raw/01_data-process.R
+++ data-raw/01_data-process.R
@@ -5,25 +5,49 @@ library(dplyr)
 # read-data ----
 seabirddiet_raw <- readxl::read_excel(here::here("data-raw", 
                                                  "Seabird Diets British Isles revised 20180524.xlsx"),
-                                      guess_max = Inf) %>% 
-    janitor::clean_names() %>%
-    bind_cols(id =  1:nrow(.), .) 
-
-
-
+                                      guess_max = Inf, na = c("", "NA")) %>% 
+    janitor::clean_names()
 
 # clean-prey-type ----
-seabirddiet <- seabirddiet_raw %>% 
+seabirddiet <- seabirddiet_raw %>%
+    recode_spp_manual(df_type = "data") %>% 
+    
     # fill missing prey type entries found in prey species
     mutate(prey_type = 
                case_when(
-                   prey_species == "0 Group Sandeel" ~ "0 Group Sandeel",
-                   prey_species == "1+ Group Sandeel" ~ "1+ Group Sandeel",
+                   prey_species == "0 Group Sandeel" ~ "Sandeel 0 Group",
+                   prey_species == "1+ Group Sandeel" ~ "Sandeel 1+ Group",
                    TRUE ~ prey_type),
            # clean prey type
            prey_type = stringr::str_to_lower(prey_type),
-           prey_type = stringr::str_replace(prey_type, "o group", "0 group"),
-           prey_type = stringr::str_replace(prey_type, "o-group", "0 group"),
+           prey_type = dplyr::recode(prey_type,
+                                     `pisces-offal` = "offal"),
+           prey_type = stringr::str_replace(prey_type, "o group|o-group|0-group", "0 group"),
+           prey_type = stringr::str_replace(prey_type, "1\\+-group|1\\+", "1+ group"),
+           prey_type = stringr::str_replace_all(prey_type, "sandeels|sandeeks|sandeel \\(ammodytes\\)|^ammodytes$", "sandeel"),
+           prey_type = stringr::str_replace(prey_type, "all ages", "all age groups"),
+           prey_type = stringr::str_replace(prey_type, "gadid.*|gadoid fish|gadoid.*", "gadidae"),
+           prey_type = stringr::str_replace(prey_type, "gadid.*|gadoid fish|gadoid.*", "gadidae"),
+           prey_type = stringr::str_replace(prey_type, "fishes|pisces", "fish"),
+           prey_type = stringr::str_replace(prey_type, "cephalopod$", "cephalopods"),
+           prey_type = stringr::str_replace(prey_type, "clupeid$|clupeiidae|clup.*ids", "clupeidae"),
+           prey_type = stringr::str_replace_all(prey_type, "polychaet.*e*.|polycheates|polychaetas", "polychaeta"),
+           prey_type = stringr::str_replace(prey_type, "gobies|goby", "gobiidae"),
+           prey_type = stringr::str_replace(prey_type, "cottidea", "cottidae"),
+           prey_type = stringr::str_replace(prey_type, "crustacean", "crustacea"),
+           prey_type = stringr::str_replace(prey_type, "eel pout", "eelpout"),
+           prey_type = stringr::str_replace_all(prey_type, "poor-cod$", "poor cod"),
+           prey_type = stringr::str_replace_all(prey_type, "rockling.*", "rockling"),
+           prey_type = stringr::str_replace_all(prey_type, "salmon.*", "salmonidae"),
+           prey_type = stringr::str_replace(prey_type, "mollusc$", "molluscs"),
+           prey_type = stringr::str_replace_all(prey_type, "(wrass sp)|(wrass$)", "wrasse"),
+           prey_type = stringr::str_replace_all(prey_type, "1 gadidae", "gadidae 1+ group"),
+           prey_type = stringr::str_replace_all(prey_type, "0 gadidae", "gadidae 0 group"),
+           prey_type = stringr::str_replace_all(prey_type, "(sprats)\\b|spart", "sprat"),
+           prey_type = stringr::str_replace_all(prey_type, "laval", "larval"),
+           prey_type = stringr::str_replace_all(prey_type, "others", "other"),
+           prey_type = stringr::str_replace_all(prey_type, "unidentified/other", "other"),
+           prey_type = stringr::str_replace_all(prey_type, "\\(|\\)|\\?|,|-", ""),
            # clean prey_species to remove
            prey_species = dplyr::recode(prey_species,
                                         `0 Group Sandeel` = "Ammodytidae",
@@ -32,12 +56,14 @@ seabirddiet <- seabirddiet_raw %>%
                                         `Sandeel 1+ Group` = "Ammodytidae",
                                         others = "other",
                                         Other = "other"),
+           age_group = dplyr::recode(age_group,
+                                        Chicks = "chick"),
            # fill in NAs
            prey_species = 
                case_when(is.na(prey_species) & 
                              stringr::str_detect(prey_type, "sandeel") ~ "Ammodytidae",
                          is.na(prey_species) & 
-                         stringr::str_detect(prey_type, "gadoids") ~ "Gadidae",
+                         stringr::str_detect(prey_type, "gadidae") ~ "Gadidae",
                          is.na(prey_species) & 
                          stringr::str_detect(prey_type, "other") ~ "Others",
                          TRUE ~ prey_species),
@@ -47,37 +73,41 @@ seabirddiet <- seabirddiet_raw %>%
                case_when(stringr::str_detect(prey_type, "0") ~ "0",
                          stringr::str_detect(prey_type, "1|(older)") ~ "+1"),
            #prey_size = stringr::str_replace_all(prey_size, "m", "")
-           )
+           # ensure start and end year are single year, not range
+           startyear = stringr::str_extract(startyear, "\\d*") %>% as.numeric(),
+           endyear = stringr::str_extract(endyear, "\\d*$") %>% as.numeric()
+           ) %>%
+    distinct() %>%
+    bind_cols(id =  1:nrow(.), .) 
 
+readr::write_csv(seabirddiet, here::here("data-raw", "intermediate", "seabirddiet.csv"))
 #source(here::here("data-raw", "01c_process_references.R"))
 
+
+# join-taxo ----
+seabirddiet <- seabirddiet %>% 
+    bind_taxonomy(role = "pred") %>% 
+    bind_taxonomy(role = "prey") 
+
+
+# join-ref ----
 ref_join <- readr::read_csv(here::here("data-raw", "joins", "ref_join.csv")) %>% 
     assertr::assert(assertr::is_uniq, reference)
-taxonomy <- readr::read_csv(here::here("data-raw", "metadata", "taxonomy.csv")) %>%
-    janitor::clean_names() %>% 
-    assertr::assert(assertr::is_uniq, base_name) 
-
-
-# join-taxo-ref ----
 seabirddiet <- seabirddiet %>% 
-    left_join(taxonomy %>% 
-                  select(base_name, base_rank, aphia_id:valid_aphia_id) %>% 
-                  setNames(paste0('prey_', names(.))), 
-              by = c("prey_species" = "prey_base_name")) %>% 
-    left_join(taxonomy %>% 
-                  select(base_name, aphia_id:valid_aphia_id) %>% 
-                  setNames(paste0('pred_', names(.))), 
-              by = c("species" = "pred_base_name")) %>%
+    df_recode_report() %>% # correct ref/notes/source column shift
     left_join(ref_join, by = "reference") %>% 
-    select(-reference)  %>%
-    assertr::verify(nrow(.) == nrow(seabirddiet_raw))
+    assertr::assert(assertr::not_na, ref_ids) %>% 
+    select(-reference) 
+
+
 
 # rename-vars ----
 # create manual recode table
 # mn_recode_tbl(tibble(names = names(seabirddiet)), names)
 mn_names <- readr::read_csv(
     here::here("data-raw", "manual_corrections",
-               "mn_seabirddiet_rename.csv")) 
+               "mn_seabirddiet_rename.csv"))
+
 seabirddiet <- seabirddiet %>% rename_at(vars(mn_names$names), ~ mn_names$rename)
 
 
@@ -88,19 +118,26 @@ seabirddiet <- seabirddiet %>%
         freq_biomass = stringr::str_extract(freq_biomass, "\\d+\\.*\\d*") %>%
             as.numeric(),
         # prey_size
-        prey_size = stringr::str_replace(prey_size, "\xf1", "+/-"))
+        prey_size = stringr::str_replace(prey_size, "\xf1|ñ", "+/-")) 
 
 
 # clean-factors
 factor_vars <- c("pred_breeding_status", "pred_age_group", "prey_age_group",
-                 "source", "sample_type", "prey_base_rank")
+                 "source", "sample_type", "prey_rank", "pred_rank")
 seabirddiet <- seabirddiet %>%
     mutate_at(factor_vars, stringr::str_to_lower) 
 
-
-
 # reorder-vars ----
 seabirddiet <- seabirddiet %>% 
+    # move variables to back for better order when using starts_with
+    dplyr::select( -pred_breeding_status,
+                   -pred_breeding_status, 
+                   -pred_age_group, 
+                   -prey_age_group, 
+                   -prey_size, 
+                   -prey_sd,
+                   everything()) %>%
+    # variable group reorder
     dplyr::select(id, year:longitude, 
                   dplyr::starts_with("pred"),
                   dplyr::starts_with("prey"),
@@ -109,6 +146,10 @@ seabirddiet <- seabirddiet %>%
                   dplyr::starts_with("ref"),
                   everything())
 
+# dataset-validate
+seabirddiet %>%
+    assertr::assert(is.numeric, year, startyear, endyear, 
+                    latitude, longitude, freq_occ, freq_num, freq_biomass) %>%
+    assertr::assert(assertr::not_na, ref_ids, prey_taxon, pred_rank) %>%
 # write-out ----
-readr::write_csv(seabirddiet, here::here("output", "seabirddiet.csv"))
-
+readr::write_csv(here::here("inst", "csv", "seabirddiet.csv"))
