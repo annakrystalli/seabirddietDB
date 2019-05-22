0d25cea8d34a1fde2d204856c1044c8a7570a7cd
annakrystalli <annakrystalli@googlemail.com>
2019-05-22 18:14:39 +0100

    add factors to seabirddiet_ & attributes

--- data-raw/02_create_metadata.R
+++ data-raw/02_create_metadata.R
@@ -1,16 +1,22 @@
+# Restart session!!!
 library(metadatar)
 library(dplyr)
+library(seabirdPrey)
 
-tbl <- readr::read_csv(here::here("inst", "csv", "seabirddiet.csv"), guess_max = Inf)
+data(seabirddiet_)
+data(seabirddiet)
+
 meta_tbl <- readr::read_csv(here::here("data-raw", "metadata", "attributes.csv"))
 
 # update meta_tbl attributeName
-meta_tbl <- mt_update_meta_tbl(tbl, meta_tbl) 
+meta_tbl <- mt_update_meta_tbl(seabirddiet, meta_tbl) 
 
 # update factors
 factor_vars <- c("pred_breeding_status", "pred_age_group", "prey_age_group",
-                 "source", "sample_type", "prey_rank")
-meta_tbl <- mt_update_factors(tbl, meta_tbl, factor_cols = factor_vars, overwrite = T) 
+                 "source", "sample_type", "prey_rank", "pred_rank", "multiyear")
+meta_tbl <- mt_update_factors(seabirddiet_, meta_tbl, factor_cols = factor_vars, overwrite = T) 
+readr::write_csv(meta_tbl, here::here("data-raw", "metadata", "attributes.csv"))
+
 
 # manual updates if required
 seabirdPrey::mn_edit_df(meta_tbl)   
