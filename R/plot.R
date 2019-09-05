4d05de7ac75d65ff18dc1f9510ddb321173b5497
annakrystalli <annakrystalli@googlemail.com>
2019-09-05 17:43:19 +0100

    harmonise plot and filter data argument to db

--- R/plot.R
+++ R/plot.R
@@ -1,8 +1,8 @@
 #' Plot predators
 #' 
 #' Plot an interactive leaflet map of preadetor diet composition.
-#' @param base_map 
-#'
+#' @param base_map plot base map. Use \code{leaflet::providers()} to get list of base map
+#' providers or visit \url{https://leaflet-extras.github.io/leaflet-providers/preview/} to preview
 #' @inheritParams sbd_filter
 #' @return plots an interactive leaflet map of the selected data
 #' @export
@@ -10,10 +10,10 @@
 #' @examples
 #' sbd_plot_predators(year = 1973)
 #' @import leaflet
-sbd_plot_predators <- function(df = seabirddietDB::seabirddiet, pred_species = NULL, prey_taxon = NULL, 
+sbd_plot_predators <- function(db = seabirddietDB::seabirddiet, pred_species = NULL, prey_taxon = NULL, 
                      year = NULL, metrics = "freq_occ", base_map = "Stamen.Watercolor"){
     
-    df <- df %>% sbd_filter(pred_species = pred_species, prey_taxon = prey_taxon, 
+    db <- db %>% sbd_filter(pred_species = pred_species, prey_taxon = prey_taxon, 
                             year = year, 
                             metrics = metrics) %>%
         dplyr::select(dplyr::one_of(c("year", "latitude", "longitude", "pred_species", 
@@ -23,13 +23,13 @@ sbd_plot_predators <- function(df = seabirddietDB::seabirddiet, pred_species = N
                        dplyr::desc(.data[[metrics[1]]])) %>%
         tidyr::nest(.key = "tabulate")
     
-    factpal <- colorFactor(rainbow(length(unique(df$pred_species))), df$pred_species)
+    factpal <- colorFactor(rainbow(length(unique(db$pred_species))), db$pred_species)
     
-    leaflet(data = df) %>% addProviderTiles(base_map) %>%
+    leaflet(data = db) %>% addProviderTiles(base_map) %>%
         addCircleMarkers(
             color = ~factpal(pred_species),
             stroke = FALSE, fillOpacity = 0.7,
-            popup = make_popup(df),
+            popup = make_popup(db),
             clusterOptions = markerClusterOptions()) %>%
         addLegend("bottomright", pal = factpal, values = ~pred_species,
                   title = "predators",
