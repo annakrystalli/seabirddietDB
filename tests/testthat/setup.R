dc950315302c5de14c5d382423f2fc4d99a35207
annakrystalli <annakrystalli@googlemail.com>
2019-09-05 15:23:10 +0100

    removes unneccessary tests & files (migrated to seabirddiet.devtools)

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,15 +1,2 @@
 context("setup.R")
 
-taxonomy <- readr::read_tsv(system.file("testdata","taxonomy.tsv", 
-                                        package="seabirdPrey")) %>%
-    dplyr::rename_all(dplyr::funs(stringr::str_replace(., "_prey", ""))) 
-
-taxo_correct <- readr::read_csv(system.file("testdata","worrms_nomatches_verified.csv", 
-                                            package="seabirdPrey"))
-
-
-seabirddiet <- readr::read_csv(system.file("testdata","data.csv", 
-                                           package="seabirdPrey")) %>% 
-    janitor::clean_names() %>%
-    dplyr::bind_cols(ID =  1:nrow(.), .)
-
--- tests/testthat/test-data-process.R
+++
@@ -1,7 +0,0 @@
-context("test-data-process.R")
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-
