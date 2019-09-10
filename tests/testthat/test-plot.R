f3cea768d86c35c9278c1b5dc2ebce674499a9fc
annakrystalli <annakrystalli@googlemail.com>
2019-09-10 16:00:04 +0100

    handle expected_output file location in testthat folder

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,12 +1,12 @@
 test_that("nesting works", {
   expect_known_output(nest_tables(seabirddiet[1:3,], metrics = "freq_occ"),
-                      file =  here::here("inst", "testdata", "nest_test.Rdata")
+                      file =  test_path( "nest_test.Rdata")
   )
 })
 
 test_that("popup works", {
     expect_known_output(make_popup(nest_tables(seabirddiet[1:3,], metrics = "freq_occ")),
-                        file =  here::here("inst", "testdata", "popup_test.Rdata")
+                        file = test_path("popup_test.Rdata")
     )
 })
 
